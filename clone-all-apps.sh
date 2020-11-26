c4f53731ba9d1db43825374f0c2ef18983398b3a
windfly <974589985@qq.com>
2020-11-26 14:13:10 +0800

    集合仓库初始化

---
+++ clone-all-apps.sh
@@ -0,0 +1,8 @@
+# 主应用
+echo "starting clone qiankun-main-app..."
+git clone https://github.com/qiankun-example/qiankun-main-app.git
+echo "end clone qiankun-main-app"
+# vue2应用
+echo "starting clone qiankun-vue2-subapp..."
+git clone https://github.com/qiankun-example/qiankun-vue2-subapp.git
+echo "end clone qiankun-vue2-subapp"
